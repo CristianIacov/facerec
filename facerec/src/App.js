ce306ef05ef6007d1b5dff2aed7e60d67fc6579f
CristianIacov <cristian.iacov1998@yahoo.com>
2019-11-30 16:22:57 +0200

    com 2

--- facerec/src/App.js
+++ facerec/src/App.js
@@ -33,10 +33,31 @@ class App extends Component{
       imageUrl:'',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        
+        id: '',
+  name: '',
+  email: '',
+
+  entries: 0,
+  joined:''
+      }
     }
   }
+loadUser= (data) =>{
+this.setState({user:
+{  
+        id: data.id,
+  name: data.name,
+  email: data.email,
+
+  entries: data.entries,
+  joined:data.joined
+
+}})}
   calculateFaceLocation = (data) => {
+
 const clarifaiFace =data.outputs[0].data.regions[0].region_info.bounding_box;
 const image = document.getElementById('inputimage');
  const width=Number(image.width);
@@ -65,7 +86,23 @@ const image = document.getElementById('inputimage');
      Clarifai.FACE_DETECT_MODEL,
      this.state.input)
     
-     .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+     .then(response =>
+     { if (response){
+      fetch('http://localhost:3001/image',{
+         method: 'put',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      id:this.state.user.id
+
+    })
+      })
+     .then(response => response.json())
+     .then(count=>{
+      this.setState(Object.assign(this.state.user,{entries:count }))
+     })
+}
+      this.displayFaceBox(this.calculateFaceLocation(response))})
+
     .catch(err => console.log(err));
   }
   
@@ -75,7 +112,7 @@ const image = document.getElementById('inputimage');
       this.setState({isSignedIn: false})
 
     }
-    else if (route === 'home ')
+    else if (route === 'home')
     {
       this.setState({isSignedIn: true})
     }
@@ -96,7 +133,7 @@ const image = document.getElementById('inputimage');
        
         <div>
      <Logo />
-     <Rank />
+     <Rank name={this.state.user.name} entries={this.state.user.entries}/>
 
     <ImageLinkForm
     onInputChange={this.onInputChange}
@@ -104,10 +141,10 @@ const image = document.getElementById('inputimage');
      />
         <FaceRecognition box={box} imageUrl={imageUrl}/>
     </div>
-     :(this.state.route === 'signin'
+     :(route === 'signin'
      ?  
-      <Signin onRouteChange={this.onRouteChange} /> 
-      :  <Register onRouteChange={this.onRouteChange} />)
+      <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
+      :  <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />)
     }
      </div>
       );
