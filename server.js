515e1c83bf9ebd76200f7bb759a9aad27995363a
CristianIacov <cristian.iacov1998@yahoo.com>
2020-05-23 19:22:48 +0300

    fixed again

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 //const profile = require('./controllers/profile');
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: 'pg',
   connection: {
    connectionString : process.env.DATABASE_URL,
-   ssl: true,
+   ssl: true
   }
 });
 
